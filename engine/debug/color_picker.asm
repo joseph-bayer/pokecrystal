a80a2c852f6a8365432bcf8980be050e367e184e
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2025-10-10 17:51:28 -0500

    Add missing dot in ellipsis

--- engine/debug/color_picker.asm
+++ engine/debug/color_picker.asm
@@ -1060,7 +1060,7 @@ DebugColor_PlaceCursor:
 DebugColor_AreYouFinishedString:
 	db   "Done?"                        ; Are you finished?
 	next "Y<DOT><DOT><DOT>", DEBUGTEST_A ; YES...(A)
-	next "N<DOT><DOT>", DEBUGTEST_B     ; NO..(B)
+	next "N<DOT><DOT><DOT>", DEBUGTEST_B ; NO..(B)
 	db   "@"
 
 DebugColor_UpArrowGFX:
